af124201669b563d1aad892c0925eada1b8df8fe
Kuba <jciesmail@gmail.com>
2021-11-04 18:50:25 +0100

    typing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
+import { LiteralArrayExpr } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'typing';
+
+export class AppComponent 
+{
+	randomText = lorem.sentence();
+	enteredText = '';
+
+	onInput(value: string)
+	{
+		this.enteredText = value;
+	}
+
+	compare(randomLetter: string, enteredLetter: string)
+	{
+		if (!enteredLetter)
+		{
+			return 'pending';
+		}
+
+		return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+	}
+
 }
